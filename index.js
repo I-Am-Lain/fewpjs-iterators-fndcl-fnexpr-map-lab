6ff5fd8db01722d08cd6998d98147838c94896e0
I-Am-Lain <vetter.william@gmail.com>
2020-11-02 23:53:31 -0600

    Done.

--- index.js
+++ index.js
@@ -11,6 +11,13 @@ const tutorials = [
   'what is JSONP?'
 ];
 
-const titleCased = () => {
-  return tutorials
-}
+function titleCased(){
+  return tutorials.map(function (tut) {   // for each tutorial
+
+
+   return tut.split(' ').map(word => {  // for each word in tutorial, new Array called NewSentence
+      return word.charAt(0).toUpperCase() + word.slice(1)  // return first character capitalized, + rest of string
+    }).join(' ')
+  
+  })
+} 
